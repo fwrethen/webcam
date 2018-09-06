3cc18c06e63ee0300bd5b374fa72ac62c68f7f01
mttronc <mrtn.mtth@gmx.de>
2018-09-06 20:41:57 +0200

    Add python timelapse script. Drop the bash script version.

---
+++ timelapse.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import shutil
+import subprocess
+
+from PIL import Image
+
+video_filename = 'timelapse.mp4'
+video_framerate = '6'
+temp_folder = 'temp'
+
+blacklist = ['cam_201806240020.jpg',
+             'cam_201807260800.jpg',
+             'cam_201807272000.jpg',
+             'cam_201807261200.jpg',
+             'cam_201808080800.jpg',
+             'cam_201808141600.jpg',
+             'cam_201808241800.jpg']
+
+filter_regex = re.compile(r'^cam_(?!\d{8}(0[0-6]00|0004|2200))[0-9]+\.jpg$')
+
+files = []
+
+with os.scandir() as it:
+    for entry in it:
+        if not entry.name.startswith('.') and entry.is_file():
+            files.append(entry.name)
+
+images = list(filter(filter_regex.search, files))
+images = list(filter(lambda x: x not in blacklist, images))
+
+
+if not os.path.exists(temp_folder):
+    os.makedirs(temp_folder)
+
+for image in images:
+    temp_img = temp_folder + '/' + image
+    date = int(re.search(r'cam_(\d{8})', image).group(1))
+
+    shutil.copyfile(image, temp_img)
+
+    crop1 = (415, 153, 1863, 968)
+    crop2 = (415, 55, 1863, 870)
+    if (date <= 20180726):
+        crop = crop1
+    else:
+        crop = crop2
+
+    im = Image.open(temp_img)
+    im_cropped = im.crop(crop)
+    im_cropped.save(temp_img, 'JPEG', quality=90)
+
+subprocess.call(['ffmpeg', '-y', '-r', video_framerate,
+                 '-pattern_type', 'glob', '-i', temp_folder+'/cam_*.jpg',
+                 '-s', 'hd720', '-vcodec', 'libx264', video_filename])
+
+shutil.rmtree(temp_folder, ignore_errors=True)
